4754ce95788b3e043ae716869605e7c2a5eff81f
jeison castañeda <jeison1995207@gmail.com>
2024-04-21 16:08:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { mongoConection } = require('./DataBases/config')
+const dotenv = require('dotenv').config()
+
+
+mongoConection()
+const app = express()
+
+
+app.get('/hola', (req, res) => {
+    return res.json({
+        msj: 'Hola'
+    })
+})
+
+
+app.get("*", (req, res) => {
+    return res.status(404).json({
+        msj: 'No encontrado',
+        status: 404
+    })
+})
+
+
+module.exports = app
+
+ 
+
---
+++ servidor.js
@@ -0,0 +1,11 @@
+const app = require('./app')
+
+
+app.set('port', process.env.PORT || 3000)
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Servidor arrancó por puerto ${app.get('port')}`)
+})
+
+
