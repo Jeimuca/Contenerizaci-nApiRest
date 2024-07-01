4485836532244b83d9e1776e180217f83ff7247b
jeison castañeda <jeison1995207@gmail.com>
2024-07-01 15:08:20 -0500

    :tada:

--- modelos/TipoProyecto.js
+++ modelos/TipoProyecto.js
@@ -1,20 +1,23 @@
+
+
 const { Schema, model } = require('mongoose');
 
 const TipoProyectoSchema = Schema({
     nombre: {
         type: String,
-        required: [true, 'Nombre es requerido'],
-        enum: ['ensayo', 'artículo', 'monografía', 'trabajo final de pregrado', 'trabajo final de especialización'],
-        unique: [true]
+        required: true
     },
     fechaCreacion: {
         type: Date,
-        default: Date.now()
+        default: Date.now
     },
     fechaActualizacion: {
         type: Date,
-        default: new Date()
+        default: null 
     }
 });
+
+module.exports = model('TipoProyecto', TipoProyectoSchema);
+
 
 module.exports = model('TipoProyecto', TipoProyectoSchema);
